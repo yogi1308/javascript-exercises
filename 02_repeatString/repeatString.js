f30627753650dced1323d903312e788cc18c2acb
Shreetej Hadge <shreetejhadge1308@gmail.com>
2024-11-26 00:50:27 -0700

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, number) {
+    if (number < 0){
+        return "ERROR"
+    }
+    finalString = ""
+    for (i = 0; i < number; ++i) {
+        finalString = finalString.concat(string)
+    }
+    return finalString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
